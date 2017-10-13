aef3a3f9f0898166952e0d41929c95f99d900b58
Kenichi Kawano <kenichi.k.820@gmail.com>
2017-10-13 17:29:03 +0900

    [add] Ver. 1.0
    ベクトル・係数を乱数を用いて決める

--- VectorLocusAndMinimumValue/Form1.Designer.vb
+++ VectorLocusAndMinimumValue/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class VectorLocusAndMinimumValue
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,193 @@ Partial Class Form1
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.createVector = New System.Windows.Forms.Button()
+        Me.numVector = New System.Windows.Forms.NumericUpDown()
+        Me.lblNumberOfVector = New System.Windows.Forms.Label()
+        Me.DrawArea = New System.Windows.Forms.PictureBox()
+        Me.Locus = New System.Windows.Forms.Button()
+        Me.numLocus = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblMinValue = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.Reset = New System.Windows.Forms.Button()
+        Me.CoefficientView = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        CType(Me.numVector, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DrawArea, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numLocus, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'createVector
+        '
+        Me.createVector.Location = New System.Drawing.Point(525, 690)
+        Me.createVector.Name = "createVector"
+        Me.createVector.Size = New System.Drawing.Size(147, 31)
+        Me.createVector.TabIndex = 0
+        Me.createVector.Text = "ベクトル生成"
+        Me.createVector.UseVisualStyleBackColor = True
+        '
+        'numVector
+        '
+        Me.numVector.Location = New System.Drawing.Point(430, 690)
+        Me.numVector.Maximum = New Decimal(New Integer() {10000000, 0, 0, 0})
+        Me.numVector.Minimum = New Decimal(New Integer() {2, 0, 0, 0})
+        Me.numVector.Name = "numVector"
+        Me.numVector.Size = New System.Drawing.Size(89, 31)
+        Me.numVector.TabIndex = 1
+        Me.numVector.Value = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'lblNumberOfVector
+        '
+        Me.lblNumberOfVector.AutoSize = True
+        Me.lblNumberOfVector.Location = New System.Drawing.Point(296, 693)
+        Me.lblNumberOfVector.Name = "lblNumberOfVector"
+        Me.lblNumberOfVector.Size = New System.Drawing.Size(128, 24)
+        Me.lblNumberOfVector.TabIndex = 2
+        Me.lblNumberOfVector.Text = "ベクトルの数"
+        '
+        'DrawArea
+        '
+        Me.DrawArea.BackColor = System.Drawing.Color.White
+        Me.DrawArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DrawArea.Location = New System.Drawing.Point(12, 12)
+        Me.DrawArea.Name = "DrawArea"
+        Me.DrawArea.Size = New System.Drawing.Size(660, 660)
+        Me.DrawArea.TabIndex = 3
+        Me.DrawArea.TabStop = False
+        '
+        'Locus
+        '
+        Me.Locus.Location = New System.Drawing.Point(525, 727)
+        Me.Locus.Name = "Locus"
+        Me.Locus.Size = New System.Drawing.Size(147, 31)
+        Me.Locus.TabIndex = 4
+        Me.Locus.Text = "軌跡の表示"
+        Me.Locus.UseVisualStyleBackColor = True
+        '
+        'numLocus
+        '
+        Me.numLocus.Increment = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.numLocus.Location = New System.Drawing.Point(430, 727)
+        Me.numLocus.Maximum = New Decimal(New Integer() {100000, 0, 0, 0})
+        Me.numLocus.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.numLocus.Name = "numLocus"
+        Me.numLocus.Size = New System.Drawing.Size(89, 31)
+        Me.numLocus.TabIndex = 5
+        Me.numLocus.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(180, 730)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(244, 24)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "合成ベクトルの軌跡の数"
+        '
+        'lblMinValue
+        '
+        Me.lblMinValue.AutoSize = True
+        Me.lblMinValue.Location = New System.Drawing.Point(432, 767)
+        Me.lblMinValue.Name = "lblMinValue"
+        Me.lblMinValue.Size = New System.Drawing.Size(87, 24)
+        Me.lblMinValue.TabIndex = 7
+        Me.lblMinValue.Text = "1.00000"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(139, 767)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(285, 24)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "合成ベクトルの長さの最小値"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.BackColor = System.Drawing.Color.Black
+        Me.ProgressBar.ForeColor = System.Drawing.Color.White
+        Me.ProgressBar.Location = New System.Drawing.Point(12, 671)
+        Me.ProgressBar.MarqueeAnimationSpeed = 50
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(660, 5)
+        Me.ProgressBar.Step = 1
+        Me.ProgressBar.TabIndex = 9
+        '
+        'Reset
+        '
+        Me.Reset.Location = New System.Drawing.Point(525, 764)
+        Me.Reset.Name = "Reset"
+        Me.Reset.Size = New System.Drawing.Size(147, 31)
+        Me.Reset.TabIndex = 10
+        Me.Reset.Text = "リセット"
+        Me.Reset.UseVisualStyleBackColor = True
+        '
+        'CoefficientView
+        '
+        Me.CoefficientView.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.CoefficientView.Location = New System.Drawing.Point(678, 12)
+        Me.CoefficientView.Name = "CoefficientView"
+        Me.CoefficientView.Size = New System.Drawing.Size(163, 783)
+        Me.CoefficientView.TabIndex = 11
+        Me.CoefficientView.UseCompatibleStateImageBehavior = False
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "係数"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "値"
+        Me.ColumnHeader2.Width = 99
+        '
+        'VectorLocusAndMinimumValue
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(853, 807)
+        Me.Controls.Add(Me.CoefficientView)
+        Me.Controls.Add(Me.Reset)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblMinValue)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.numLocus)
+        Me.Controls.Add(Me.Locus)
+        Me.Controls.Add(Me.lblNumberOfVector)
+        Me.Controls.Add(Me.numVector)
+        Me.Controls.Add(Me.createVector)
+        Me.Controls.Add(Me.DrawArea)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Font = New System.Drawing.Font("MS UI Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.MaximizeBox = False
+        Me.Name = "VectorLocusAndMinimumValue"
+        Me.Text = "VectorLocusAndMinimumValue"
+        CType(Me.numVector, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DrawArea, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numLocus, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents createVector As Button
+    Friend WithEvents numVector As NumericUpDown
+    Friend WithEvents lblNumberOfVector As Label
+    Friend WithEvents DrawArea As PictureBox
+    Friend WithEvents Locus As Button
+    Friend WithEvents numLocus As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMinValue As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar As ProgressBar
+    Friend WithEvents Reset As Button
+    Friend WithEvents CoefficientView As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
 End Class
--- VectorLocusAndMinimumValue/Form1.vb
+++ VectorLocusAndMinimumValue/Form1.vb
@@ -1,3 +1,283 @@
-﻿Public Class Form1
+﻿Public Class VectorLocusAndMinimumValue
+    Const scale_size As Integer = 300
+    Const valid_digits As String = "0.00000"
+    Const lowest_nest As Integer = 1
 
+    Dim rnd As New Random
+
+    Dim origin As Point
+
+    Dim theta() As Double
+    Dim vector() As PointF
+
+    Dim boundary() As Double ' coefficientを求めるために利用
+    Dim coefficient() As Double
+
+    ' -------------------------------------------------------------------------
+    ' 合成ベクトルの最小値を保存するクラス
+    ' -------------------------------------------------------------------------
+    Public Class MinimumValue
+        Dim min_value As Double = 1
+        Dim min_coefficient() As Double
+
+        Property Value
+            Get
+                Return min_value
+            End Get
+            Set(value)
+                min_value = value
+            End Set
+        End Property
+
+        ReadOnly Property Condition(ByVal i As Integer)
+            Get
+                Return min_coefficient(i)
+            End Get
+        End Property
+
+        Public Sub PreserveCoefficient(ByVal i As Integer, ByVal v As Double)
+            Me.min_coefficient(i) = v
+        End Sub
+
+        Public Sub Realloc(ByVal size As Integer)
+            ReDim Me.min_coefficient(size)
+        End Sub
+    End Class
+
+    Dim min_value As New MinimumValue
+
+
+    ' =========================================================================
+    ' イベント
+    ' =========================================================================
+    Private Sub VectorLocusAndMinimumValue_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 原点の座標を設定
+        origin = New Point(DrawArea.Width / 2, DrawArea.Height / 2)
+
+        ' 描画エリアのインスタンスを作成
+        Me.DrawArea.Image = New Bitmap(Me.DrawArea.Width, Me.DrawArea.Height)
+
+        ' ListView を詳細表示に設定
+        Me.CoefficientView.View = View.Details
+
+        UpdateData()
+
+        For i As Integer = 0 To numVector.Value - 1
+            DrawVector(i)
+            DrawArea.Refresh()
+
+            Me.CoefficientView.Items.Add("C_" & i + 1, i)
+            Me.CoefficientView.Items(i).SubItems.Add(Format(min_value.Condition(i), valid_digits))
+        Next
+
+        AddHandler DrawArea.Paint, AddressOf Me.Origin_Paint
+    End Sub
+
+
+    Private Sub CreateVector_Click(sender As Object, e As EventArgs) Handles createVector.Click
+        UpdateData()
+
+        Me.CoefficientView.Items.Clear()
+        ProgressBar.Maximum = numVector.Value - 1
+        For i As Integer = 0 To numVector.Value - 1
+            ProgressBar.Value = i
+            DrawVector(i)
+            DrawArea.Refresh()
+
+            Me.CoefficientView.Items.Add("C_" & i + 1, i)
+            Me.CoefficientView.Items(i).SubItems.Add(Format(Me.coefficient(i), valid_digits))
+
+        Next
+    End Sub
+
+
+    Private Sub Locus_Click(sender As Object, e As EventArgs) Handles Locus.Click
+        ProgressBar.Maximum = numLocus.Value - 1
+        For i As Integer = 0 To numLocus.Value - 1
+            ProgressBar.Value = i
+            DecideCoefficient()
+            DrawPoint()
+            DrawArea.Refresh()
+            lblMinValue.Text = Format(min_value.Value, valid_digits)
+        Next
+
+        For i As Integer = 0 To numVector.Value - 1
+            Me.CoefficientView.Items(i).SubItems(1).Text = Format(min_value.Condition(i), valid_digits)
+        Next
+    End Sub
+
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Reset.Click
+        numVector.Value = numVector.Minimum
+        numLocus.Value = numLocus.Minimum
+
+        UpdateData()
+
+        Me.CoefficientView.Items.Clear()
+        ProgressBar.Maximum = numVector.Value - 1
+        For i As Integer = 0 To numVector.Value - 1
+            ProgressBar.Value = i
+            DrawVector(i)
+            DrawArea.Refresh()
+
+            Me.CoefficientView.Items.Add("C_" & i + 1, i)
+            Me.CoefficientView.Items(i).SubItems.Add(Format(Me.coefficient(i), valid_digits))
+        Next
+    End Sub
+
+
+    ' =========================================================================
+    ' 描画系サブルーチン
+    ' =========================================================================
+
+    Private Sub Origin_Paint(ByVal sender As Object, ByVal e As PaintEventArgs)
+        Dim g As Graphics = e.Graphics
+        g.FillRectangle(Brushes.Orange, Me.origin.X - 3, Me.origin.Y - 3, 6, 6)
+    End Sub
+
+    Private Sub DrawVector(ByVal i As Integer)
+        Dim g As Graphics = Graphics.FromImage(Me.DrawArea.Image)
+
+        Dim dst_x As Integer = CInt(Me.origin.X + scale_size * Me.vector(i).X)
+        Dim dst_y As Integer = CInt(Me.origin.Y + scale_size * Me.vector(i).Y)
+
+        g.DrawLine(Pens.Black, Me.origin.X, Me.origin.Y, dst_x, dst_y)
+    End Sub
+
+
+    Private Sub DrawPoint()
+        Dim g As Graphics = Graphics.FromImage(Me.DrawArea.Image)
+
+        Dim sum_vector As New PointF(0, 0)
+        For i As Integer = 0 To Me.numVector.Value - 1
+            sum_vector.X += Me.coefficient(i) * Me.vector(i).X
+            sum_vector.Y += Me.coefficient(i) * Me.vector(i).Y
+        Next
+
+        Dim dst_x As Integer = CInt(Me.origin.X + scale_size * sum_vector.X)
+        Dim dst_y As Integer = CInt(Me.origin.Y + scale_size * sum_vector.Y)
+
+        Dim distance As Double = Math.Pow(sum_vector.X, 2) + Math.Pow(sum_vector.Y, 2)
+        If min_value.Value > distance Then
+            min_value.Value = distance
+            For i As Integer = 0 To Me.numVector.Value - 1
+                min_value.PreserveCoefficient(i, Me.coefficient(i))
+            Next
+        End If
+
+        g.DrawRectangle(Pens.Red, dst_x, dst_y, 1, 1)
+    End Sub
+
+
+    ' =========================================================================
+    ' その他のサブルーチン
+    ' =========================================================================
+
+    Private Sub Realloc(Of T)(ByRef ary() As T, ByVal size As Integer)
+        ReDim ary(size)
+    End Sub
+
+
+    Private Sub Randomize(ByRef ary() As Double)
+        For i As Integer = 0 To ary.Length - 1
+            ary(i) = 2 * Math.PI * rnd.NextDouble
+        Next
+    End Sub
+
+
+    Private Sub DicideElement()
+        For i As Integer = 0 To Me.vector.Length - 1
+            Me.vector(i).X = Math.Cos(Me.theta(i))
+            Me.vector(i).Y = Math.Sin(Me.theta(i))
+        Next
+    End Sub
+
+
+    Private Sub DuplicateCombination(ByVal idx_a As Integer, ByVal idx_b As Integer, ByVal nest As Integer)
+        If nest < lowest_nest Then
+            Return
+        End If
+
+        boundary(idx_a) = 0
+        While (boundary(idx_a) <= boundary(idx_b))
+            DuplicateCombination(idx_a + 1, idx_a, nest - 1)
+
+            If nest = lowest_nest Then
+                For i As Integer = 0 To numVector.Value - 2
+                    coefficient(i) = boundary(i) - boundary(i + 1)
+                Next
+                coefficient(numVector.Value - 1) = boundary(numVector.Value - 1)
+
+                DrawPoint()
+                DrawArea.Refresh()
+            End If
+
+            boundary(idx_a) += 1
+        End While
+    End Sub
+
+
+    Private Sub DecideCoefficient()
+        Dim sum_boundary As Integer = 0
+
+        ' 乱数生成 Ver. 1
+        'For i As Integer = 0 To Me.numVector.Value - 2
+        '    tmp_i = rnd.Next(tmp_i, 100000000)
+        '    Me.boundary(i) = tmp_i / 100000000
+        'Next
+        'Me.boundary(numVector.Value - 1) = 1
+
+        'Dim tmp_d As Double = 0
+        'For i As Integer = 0 To Me.numVector.Value - 1
+        '    Me.coefficient(i) = Me.boundary(i) - tmp_d
+        '    tmp_d = Me.boundary(i)
+        'Next
+
+
+        ' 乱数生成 Ver. 2
+        While sum_boundary = 0
+            For i As Integer = 0 To Me.numVector.Value - 1
+                Me.boundary(i) = rnd.Next(10000000)
+                sum_boundary += Me.boundary(i)
+            Next
+        End While
+
+        For i As Integer = 0 To Me.numVector.Value - 1
+            Me.coefficient(i) = Me.boundary(i) / sum_boundary
+        Next
+
+
+        ' 乱数生成 Ver. 3
+        'boundary(0) = 100
+
+        'DuplicateCombination(1, 0, numVector.Value - 1)
+
+
+        'Me.DrawPoint()
+    End Sub
+
+
+    ' =========================================================================
+    ' 一連の処理
+    ' =========================================================================
+
+    Private Sub UpdateData()
+        Me.Realloc(Me.theta, Me.numVector.Value)
+        Me.Randomize(Me.theta)
+
+        Realloc(Me.vector, Me.numVector.Value)
+        DicideElement()
+
+        Realloc(Me.boundary, Me.numVector.Value)
+        Realloc(Me.coefficient, Me.numVector.Value)
+        DecideCoefficient()
+
+        Me.DrawArea.Image.Dispose()
+        Me.DrawArea.Image = New Bitmap(Me.DrawArea.Width, Me.DrawArea.Height)
+
+        min_value.Value = 1
+        min_value.Realloc(Me.numVector.Value)
+
+        Me.lblMinValue.Text = Format(1, valid_digits)
+    End Sub
 End Class
